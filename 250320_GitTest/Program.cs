3b12ed4469c49294c4651e7fff1f3705a73fb9d3
2000yhlee <backtoback0320@gmail.com>
2025-03-20 12:42:26 +0900

    Feat New task
    새로운 기능 추가

--- 250320_GitTest/Program.cs
+++ 250320_GitTest/Program.cs
@@ -10,6 +10,10 @@ namespace _250320_GitTest
     {
         static void Main(string[] args)
         {
+            Console.Write("이름을 입력해주세요 : ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("당신의 이름은 {0}입니다.", input);
         }
     }
 }
